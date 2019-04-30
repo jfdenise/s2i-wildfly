e11b40756cd6910d333fd919cf88f72fbcbac99f
Jean-Francois Denise <jfdenise@gmail.com>
2019-04-30 15:52:30 +0200

    single parent for descriptions and feature-pack, updated offliner with latest wildfly

--- cekit/wildfly-modules/jboss/container/wildfly/galleon/configure.sh
+++ cekit/wildfly-modules/jboss/container/wildfly/galleon/configure.sh
@@ -19,13 +19,18 @@ rm /tmp/offliner.jar
 # required to have maven enabled.
 source $JBOSS_CONTAINER_MAVEN_35_MODULE/scl-enable-maven
 
-mvn -f $SCRIPT_DIR/wildfly-s2i-galleon-pack/pom.xml install -Dmaven.repo.local=$MAVEN_LOCAL_REPO
+# Build WildFly s2i feature-pack and install it in local maven repository
+mvn -f $JBOSS_CONTAINER_WILDFLY_GALLEON/wildfly-s2i-galleon-pack/pom.xml install -Dmaven.repo.local=$MAVEN_LOCAL_REPO
 
-DEFAULT_SERVER=os-standalone-profile
+# Remove the feature-pack src
+rm -rf $JBOSS_CONTAINER_WILDFLY_GALLEON/wildfly-s2i-galleon-pack
 
-mvn -f $JBOSS_CONTAINER_WILDFLY_GALLEON_DEFINITIONS/$DEFAULT_SERVER/pom.xml package -Dmaven.repo.local=$MAVEN_LOCAL_REPO -Dcom.redhat.xpaas.repo.jbossorg --settings $HOME/.m2/settings.xml
+# Provision the default server
+DEFAULT_SERVER=definitions/os-standalone-profile
+mvn -f $JBOSS_CONTAINER_WILDFLY_GALLEON/$DEFAULT_SERVER/pom.xml package -Dmaven.repo.local=$MAVEN_LOCAL_REPO -Dcom.redhat.xpaas.repo.jbossorg --settings $HOME/.m2/settings.xml
 
-cp -r $JBOSS_CONTAINER_WILDFLY_GALLEON_DEFINITIONS/$DEFAULT_SERVER/target/wildfly $JBOSS_HOME && rm -r $JBOSS_CONTAINER_WILDFLY_GALLEON_DEFINITIONS/$DEFAULT_SERVER/target && \
+# Install WildFly server
+cp -r $JBOSS_CONTAINER_WILDFLY_GALLEON/$DEFAULT_SERVER/target/wildfly $JBOSS_HOME && rm -r $JBOSS_CONTAINER_WILDFLY_GALLEON/$DEFAULT_SERVER/target && \
     ln -s $JBOSS_HOME /wildfly
 cp -r $JBOSS_HOME/standalone/deployments/* /deployments
 rm -rf $JBOSS_HOME/standalone/deployments
@@ -34,4 +39,4 @@ ln -s /deployments $JBOSS_HOME/standalone/deployments
 chown -R 1001:0 $JBOSS_HOME && chmod -R ug+rwX $JBOSS_HOME 
 chown -R 1001:0 $HOME
 chmod -R ug+rwX $MAVEN_LOCAL_REPO
-chmod -R ug+rw $JBOSS_CONTAINER_WILDFLY_GALLEON_DEFINITIONS
+chmod -R ug+rw $JBOSS_CONTAINER_WILDFLY_GALLEON
