a896d33d09d4d3cd218a1f5941361a128f2f15a4
JF Denise <jdenise@redhat.com>
2019-05-10 17:34:28 +0200

    fix config file prop for all modules

--- cekit/wildfly-modules/jboss/container/wildfly/os-wildfly-launch/artifacts/launch/openshift-common.sh
+++ cekit/wildfly-modules/jboss/container/wildfly/os-wildfly-launch/artifacts/launch/openshift-common.sh
@@ -6,7 +6,8 @@ if [ "${SCRIPT_DEBUG}" = "true" ] ; then
     echo "Script debugging is enabled, allowing bash commands and their arguments to be printed as they are executed"
 fi
 
-CONFIG_FILE=$JBOSS_HOME/standalone/configuration/${SERVER_CONFIGURATION}
+SERVER_CONFIG=${WILDFLY_SERVER_CONFIGURATION:-standalone.xml}
+CONFIG_FILE=$JBOSS_HOME/standalone/configuration/${SERVER_CONFIG}
 LOGGING_FILE=$JBOSS_HOME/standalone/configuration/logging.properties
 
 CONFIGURE_SCRIPTS=(
